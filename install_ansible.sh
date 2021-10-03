ad2ed881f15c2c50f08bd92299c023b2f68cb862
Artem Pavlichenko <artem.pvl@gmail.com>
2021-10-04 02:41:45 +0300

    pip

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,4 +5,5 @@ apt install python3-pip -y
 apt install software-properties-common -y
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
+pip install packaging
 pip install docker
