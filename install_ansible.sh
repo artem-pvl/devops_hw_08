f0070036d972e4519253409e61a8c116dc1076c1
Artem Pavlichenko <artem.pvl@gmail.com>
2021-10-03 18:29:41 +0300

    first commit

---
+++ install_ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update -y
+apt install python3 -y
+apt install software-properties-common -y
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
+pip install docker
